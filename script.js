b9a14cb9cec7155b26e69257eb338179d608e76b
AugustRene <official.makaylarene@gmail.com>
2026-07-17 20:19:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+//Creates a drop down and hide function for the Ethos tab section
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
